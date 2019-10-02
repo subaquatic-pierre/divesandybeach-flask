2858aaf2d075f87c6fdf8f3d60b3163b9c0414b2
subaquatic-pierre <subuatic.pierre@gmail.com>
2019-10-02 11:42:54 +0400

    yes

--- application.py
+++ application.py
@@ -1,5 +1,11 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import  RegistrationForm, LoginForm
+
+# Application variable assigned to Flask object
 app = Flask(__name__)
+
+# Set secret code for appliaction to prevent CSRF token (cross site request forgery token) used by WTForms, should set environment vairable
+app.config['SECRET_KEY'] = 'd30cdb1c8d3e69f91295902e044d5333'
 
 posts = [
     {
@@ -16,6 +22,7 @@ posts = [
     }
 ]
 
+
 @app.route('/')
 @app.route('/home')
 def home():
@@ -25,6 +32,30 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        flash(f'Acount created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash(f'You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessfull. Please check username and password', 'danger')
+
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
