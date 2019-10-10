ffad3ba42e37c10241938c875fd851ea5ac8f2e9
subaquatic-pierre <subuatic.pierre@gmail.com>
2019-10-10 14:35:50 +0400

    config file

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -7,10 +7,10 @@ class Config:
     MAIL_USE_TLS = True
 
     # Set enviroment variables for username and password
-    MAIL_USERNAME = 'subaquatic.pierre@gmail.com'
-    MAIL_PASSWORD = 'M0ther!and'
+    MAIL_USERNAME = os.environ.get['GMAIL_USERNAME']
+    MAIL_PASSWORD = os.environ.get['GMAIL_PASSWORD']
     # Set secret code for application to prevent CSRF token (cross site request forgery token) used by WTForms, should set environment vairable
-    SECRET_KEY = 'd30cdb1c8d3e69f91295902e044d5333'
+    SECRET_KEY = os.environ.get['FLASKBLOG_SECRET_KEY']
 
     # Database setup
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
