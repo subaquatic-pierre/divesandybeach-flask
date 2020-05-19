75e045383da137d005e03379f2c9f86bdb8ea027
subaquatic-pierre <subaquatic.pierre@gmail.com>
2020-05-19 21:26:05 +0400

    add dummy user

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,7 +1,8 @@
 import os
 
+
 class Config:
-    # Mail settings to send emails 
+    # Mail settings to send emails
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
@@ -10,7 +11,7 @@ class Config:
     MAIL_USERNAME = os.environ.get('GMAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('GMAIL_PASSWORD')
     # Set secret code for application to prevent CSRF token (cross site request forgery token) used by WTForms, should set environment vairable
-    SECRET_KEY = os.environ.get('FLASKBLOG_SECRET_KEY')
+    SECRET_KEY = "supersecrteteky"
 
     # Database setup
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
